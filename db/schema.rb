d6bd3eac10c4d249f1618f62bd2344692b0b4dfc
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2024-03-02 10:03:02 +0900

    setting about location

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_26_232740) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_02_005620) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -41,6 +41,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_26_232740) do
 
   create_table "cats", force: :cascade do |t|
     t.string "nickname", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.decimal "latitude", precision: 11, scale: 8
+    t.decimal "longitude", precision: 11, scale: 8
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
